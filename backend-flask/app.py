401a1d470f618c034ae575f2abc16155d6dd9000
AfroLatino <ogh.peta@gmail.com>
2023-03-08 10:22:33 +0000

    cognito updates

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -177,13 +177,13 @@ def data_home():
  access_token = extract_access_token(request.headers)
  try:
   claims = cognito_jwt_token.verify(access_token)
-        #athenticated request
+        #authenticated request
   app.logger.debug("authenticated")
   app.logger.debug(claims)
-      #app.logger.debug(claims['username'])
-  data = HomeActivities.run()
+  app.logger.debug(claims['username'])
+  data = HomeActivities.run(cognito_user_id=claims['username'])
  except TokenVerifyError as e:
-      #authenticated request
+      #unauthenticated request
   app.logger.debug(e)
   app.logger.debug("unauthenticated")
   data = HomeActivities.run()  
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,7 +5,7 @@ import logging
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run(): 
+  def run(cognito_user_id=None): 
     #logger.info("HomeActivities")
     ## HoneyComb Span addition
     with tracer.start_as_current_span("http-handler") as outer_span:
@@ -15,6 +15,7 @@ class HomeActivities:
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("user.id", 'AfroLatino')
       span.set_attribute("app.now", now.isoformat())  
+
       results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
@@ -55,6 +56,19 @@ class HomeActivities:
       'replies': []
      }
      ]
+
+    if cognito_user_id != None:
+      extra_crud = {
+        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+        'handle':  'Garek',
+        'message': 'My dear doctor, I am just simple tailor',
+        'created_at': (now - timedelta(hours=1)).isoformat(),
+        'expires_at': (now + timedelta(hours=12)).isoformat(),
+        'likes': 0,
+        'replies': []
+      }
+      results.insert(0,extra_crud)
+
     span.set_attribute("app.result_length", len(results))   
     return results
   
