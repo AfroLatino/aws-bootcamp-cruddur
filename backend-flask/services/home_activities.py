15671b760888d5f1a4b6eed4120f9c5a86a97f62
AfroLatino <ogh.peta@gmail.com>
2023-03-08 10:33:45 +0000

    implement back-end check for the token

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -60,11 +60,11 @@ class HomeActivities:
     if cognito_user_id != None:
       extra_crud = {
         'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Garek',
-        'message': 'My dear doctor, I am just simple tailor',
+        'handle':  'Lore',
+        'message': 'My dear brother, it is the humans that are the problem',
         'created_at': (now - timedelta(hours=1)).isoformat(),
         'expires_at': (now + timedelta(hours=12)).isoformat(),
-        'likes': 0,
+        'likes': 1042,
         'replies': []
       }
       results.insert(0,extra_crud)
