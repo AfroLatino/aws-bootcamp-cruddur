328d2c77252a17a6f61fbfdd72873281d9dc7b14
AfrLatino L <o.cube7@live.com>
2023-03-28 18:33:29 +0000

    updated dockerfile and app.py, created health-check and test file

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -85,6 +85,11 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST",
 )
 
+#Health check added
+@app.route('/api/health-check')
+def health_check():
+  return {'success': True}, 200
+  
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
   access_token = extract_access_token(request.headers)
