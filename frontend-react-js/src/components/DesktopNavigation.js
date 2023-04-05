260cfa9577aef7cac11ea487824c9bdc00b10b82
AroLatino A <o.cube7@live.com>
2023-04-05 19:10:26 +0000

    made changes to desktop navigation

--- frontend-react-js/src/components/DesktopNavigation.js
+++ frontend-react-js/src/components/DesktopNavigation.js
@@ -25,7 +25,7 @@ export default function DesktopNavigation(props) {
       handle="messages" 
       active={props.active} />
     profileLink = <DesktopNavigationLink 
-      url="/@afrolatino" 
+      url="/@:handle"
       name="Profile"
       handle="profile"
       active={props.active} />
